414d290d0f17a02b290ae600b34f709419a2933e
Juan Luis Rastrollo Guerrero <juanlu@MBP-de-Juan.home>
2020-03-17 13:49:50 +0100

    Final version

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -6,5 +6,31 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'pipes';
+  nombre = 'Capitán América';
+  nombre2 = 'cApiTÁN amÉRIcA';
+  array = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10];
+  PI: number =  Math.PI;
+  porcentaje = 0.235;
+  salario = 1234.5;
+  fecha: Date = new Date();
+  activar: boolean = false;
+  idioma = 'es';
+  videoUrl = 'https://www.youtube.com/embed/REEOyXVdjBQ';
+
+  valorPromesa = new Promise<string>((resolve) => {
+    setTimeout(() => {
+      resolve('Llego el dato');
+    }, 4500);
+  });
+
+  heroe = {
+    nombre: 'Logan',
+    clave: 'Wolverine',
+    edad: 500,
+    direccion: {
+      calle: 'Primera',
+      casa: 20
+    }
+  };
+
 }
